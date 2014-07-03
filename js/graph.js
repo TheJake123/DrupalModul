d831fad697214901da89de111a3c8f50e5d68727
TheJake123 <zebra_streif@hotmail.com>
2014-07-03 17:33:24 +0200

    -Fixed graph.js loadup

--- js/graph.js
+++ js/graph.js
@@ -2,60 +2,77 @@ var kurse = {}, toplevel = [];
 
 /**
  * Gets list of all courses from test JSON file and sets up event handlers
-**/
-jQuery(document).ready(function () {
-	var head  = jQuery('head')[0];
-	var link  = document.createElement('link');
-	link.rel  = 'stylesheet';
-	link.type = 'text/css';
-	link.href = 'sites/all/modules/stukowin/css/curriculum_style.css';
-	link.media = 'all';
-	head.appendChild(link);
-	
-	var type = jQuery("#curriculum_display").data("currtype");
-	var currList = jQuery("#curriculum_display").data("curriculums").split(" ");
-	var reqUrl = "http://sir.profflasche.at:8081/drupal/?q=stukowin/crclmlst";
-	reqUrl = buildRequest(reqUrl, type, currList);
-	
-	jQuery.ajax({
-		url: reqUrl,
-		dataType: 'json',
-		success: function (result) {
-			gc(result);
-		},
-		error: function (request, textStatus, errorThrown) {
-			alert(textStatus + ": " + errorThrown);
-			alert(request.status);
-		}
-	});
+ */
+jQuery(document)
+		.ready(
+				function() {
+					var head = jQuery('head')[0];
+					var link = document.createElement('link');
+					link.rel = 'stylesheet';
+					link.type = 'text/css';
+					link.href = 'sites/all/modules/stukowin/css/curriculum_style.css';
+					link.media = 'all';
+					head.appendChild(link);
 
-	jQuery("#curriculum_display").on("click", ".button", function (event) {
-		if (jQuery(this).hasClass("expander")) {
-			expand_reduce(jQuery(this).closest("div.fach, div.modul"));
-		} else if (jQuery(this).hasClass("empfohlen") && !(jQuery(this).hasClass("empty"))) {
-			showEmpfohlen(jQuery(this).closest("div.fach, div.modul, div.lv"));
-			jQuery(this).toggleClass("active");
-		} else if (jQuery(this).hasClass("voraussetzung") && !(jQuery(this).hasClass("empty"))) {
-			showVoraussetzungen(jQuery(this).closest("div.fach, div.modul, div.lv"));
-			jQuery(this).toggleClass("active");
-		}
-	});
-	jQuery("#curriculum_display").on("click", "a.bedingung", function (event) {
-		if (jQuery(this).hasClass("voraussetzung")) {
-			expandAndScrollToElement(jQuery(this).attr("id"), "#fc6554");
-		} else if (jQuery(this).hasClass("empfohlen")) {
-			expandAndScrollToElement(jQuery(this).attr("id"), "#8ad758");
-		}
-		return false;
-	});
+					var type = jQuery("#curriculum_display").data("currtype");
+					var currList = jQuery("#curriculum_display").data(
+							"curriculums").split(" ");
+					var reqUrl = "http://sir.profflasche.at:8081/drupal/?q=stukowin/crclmlst";
+					reqUrl = buildRequest(reqUrl, type, currList);
 
-});
+					jQuery.ajax({
+						url : reqUrl,
+						dataType : 'json',
+						success : function(result) {
+							gc(result);
+						},
+						error : function(request, textStatus, errorThrown) {
+							alert(textStatus + ": " + errorThrown);
+							alert(request.status);
+						}
+					});
+
+					jQuery("#curriculum_display").on(
+							"click",
+							".button",
+							function(event) {
+								if (jQuery(this).hasClass("expander")) {
+									expand_reduce(jQuery(this).closest(
+											"div.fach, div.modul"));
+								} else if (jQuery(this).hasClass("empfohlen")
+										&& !(jQuery(this).hasClass("empty"))) {
+									showEmpfohlen(jQuery(this).closest(
+											"div.fach, div.modul, div.lv"));
+									jQuery(this).toggleClass("active");
+								} else if (jQuery(this).hasClass(
+										"voraussetzung")
+										&& !(jQuery(this).hasClass("empty"))) {
+									showVoraussetzungen(jQuery(this).closest(
+											"div.fach, div.modul, div.lv"));
+									jQuery(this).toggleClass("active");
+								}
+							});
+					jQuery("#curriculum_display").on(
+							"click",
+							"a.bedingung",
+							function(event) {
+								if (jQuery(this).hasClass("voraussetzung")) {
+									expandAndScrollToElement(jQuery(this).attr(
+											"id"), "#fc6554");
+								} else if (jQuery(this).hasClass("empfohlen")) {
+									expandAndScrollToElement(jQuery(this).attr(
+											"id"), "#8ad758");
+								}
+								return false;
+							});
+
+				});
 
 /**
- * Extracts the matching curriculum out of the JSON-data and calls fill_crclm
- * @param {O
- * bject} data All curriculums to search
-**/
+ * Extracts the matching curricula out of the JSON-data and calls fill_crclm
+ * 
+ * @param {Object} data All curriculums to search
+ */
 
 function gc(data) {
 	jQuery("#curriculum_display");
@@ -66,37 +83,50 @@ function gc(data) {
 		option.value = data[i]["vid"];
 		option.textContent = data[i]["name"];
 		select.appendChild(option);
-	};
-	select.onchange=function(){
+	}
+	;
+	select.onchange = function() {
 		var selectElem = document.getElementById("curr_select");
 		var currId = selectElem.options[selectElem.selectedIndex].value;
-		jQuery.getJSON("http://sir.profflasche.at:8081/drupal/?q=stukowin/crclm/" + currId, fill_crclm);
+		jQuery.getJSON(
+				"http://sir.profflasche.at:8081/drupal/?q=stukowin/crclm/"
+						+ currId, fill_crclm);
 		clearDiv();
 	};
 	jQuery("#curriculum_display").append(select);
-	jQuery.getJSON("http://sir.profflasche.at:8081/drupal/?q=stukowin/crclm/" + data[0]["vid"], fill_crclm);
+	jQuery.getJSON("http://sir.profflasche.at:8081/drupal/?q=stukowin/crclm/"
+			+ data[0]["vid"], fill_crclm);
 }
 
 /**
- * Extracts the top-level courses out of the JSON-data and fills the page with content
- * @param {Object} data The curriculum data to display
-**/
+ * Extracts the top-level courses out of the JSON-data and fills the page with
+ * content
+ * 
+ * @param {Object}
+ *            data The curriculum data to display
+ */
 function fill_crclm(data) {
 	for (var i = 0; i < data.length; i++) {
 		kurse[data[i]["tid"]] = data[i];
-	};
-	for (var key in kurse) {
+	}
+	;
+	for ( var key in kurse) {
 		jQuery("#curriculum_display").append(createDivs(kurse[key], 0));
-	};
-	//expand_all();
+	}
+	;
+	reduce_all();
 }
 
 /**
  * Shows or hides the list of recommended courses for a given course
- * @param {Object} element The element to show the recommended courses for
-**/
+ * 
+ * @param {Object}
+ *            element The element to show the recommended courses for
+ */
 function showEmpfohlen(element) {
-	if (typeof element === "undefined" || !(typeof element === "object") || !(element.prop("tagName") == "DIV")) return;
+	if (typeof element === "undefined" || !(typeof element === "object")
+			|| !(element.prop("tagName") == "DIV"))
+		return;
 	element.children("div.bedingung.empfohlen").remove();
 	if (element.children("table.header").find("td.button.empfohlen.active").length) {
 		return;
@@ -105,9 +135,19 @@ function showEmpfohlen(element) {
 	if ("empfohlen" in kurse[id]["lva"]) {
 		var list = '<div class="bedingung empfohlen">';
 		list += '<p class="bedingung empfohlen">Für diesen Kurs sind folgende Kurse empfohlen:</p><ul class="bedingung empfohlen">';
-		jQuery.each(kurse[id]["lva"]["empfohlen"], function (key, val) {
-			list += '<li><a href="javascript:void(0)"   class="bedingung empfohlen" id="' + val + '">' + ("lvtypshort" in kurse[val]["lva"] ? kurse[val]["lva"]["lvtypshort"] + " " : "") + kurse[val]["lva"]["title"] + "</a></li>";
-		});
+		jQuery
+				.each(
+						kurse[id]["lva"]["empfohlen"],
+						function(key, val) {
+							list += '<li><a href="javascript:void(0)"   class="bedingung empfohlen" id="'
+									+ val
+									+ '">'
+									+ ("lvtypshort" in kurse[val]["lva"] ? kurse[val]["lva"]["lvtypshort"]
+											+ " "
+											: "")
+									+ kurse[val]["lva"]["title"]
+									+ "</a></li>";
+						});
 		list += "</ul>";
 		list += '</div>';
 		element.append(list);
@@ -116,23 +156,42 @@ function showEmpfohlen(element) {
 
 /**
  * Shows or hides the list of required courses for a given course
- * @param {Object} element The element to show the required courses for
-**/
+ * 
+ * @param {Object}
+ *            element The element to show the required courses for
+ */
 function showVoraussetzungen(element) {
-	if (typeof element === "undefined" || !(typeof element === "object") || !(element.prop("tagName") == "DIV")) return;
+	if (typeof element === "undefined" || !(typeof element === "object")
+			|| !(element.prop("tagName") == "DIV"))
+		return;
 	element.children("div.bedingung.voraussetzung").remove();
 	if (element.children("table.header").find("td.button.voraussetzung.active").length) {
 		return;
 	}
 	var id = element.attr("id");
-	if ("vorraussetzung" in kurse[id]["lva"]) { // Falsch geschrieben in Testphase (Fabian der alte Spanier hat ein rollendes R^^)
+	if ("vorraussetzung" in kurse[id]["lva"]) { // Falsch geschrieben in
+		// Testphase (Fabian der alte
+		// Spanier hat ein rollendes
+		// R^^)
 		var list = '<div class="bedingung voraussetzung">';
 		list += '<p class="bedingung voraussetzung">Folgende Kurse müssen verpflichtend vor diesem Kurs durchgeführt werden:</p><ul class="bedingung voraussetzung">';
-		jQuery.each(kurse[id]["lva"]["vorraussetzung"], function (key, val) { // Falsch geschrieben (s.o.)
-			if (val in kurse) {
-				list += '<li><a href="javascript:void(0)" class="bedingung voraussetzung" id="' + val + '">' + ("lvtypshort" in kurse[val] ? kurse[val]["lva"]["lvtypshort"] + " " : "") + kurse[val]["lva"]["title"] + "</a></li>";
-			}
-		});
+		jQuery
+				.each(
+						kurse[id]["lva"]["vorraussetzung"],
+						function(key, val) { // Falsch
+							// geschrieben
+							// (s.o.)
+							if (val in kurse) {
+								list += '<li><a href="javascript:void(0)" class="bedingung voraussetzung" id="'
+										+ val
+										+ '">'
+										+ ("lvtypshort" in kurse[val] ? kurse[val]["lva"]["lvtypshort"]
+												+ " "
+												: "")
+										+ kurse[val]["lva"]["title"]
+										+ "</a></li>";
+							}
+						});
 		list += "</ul>";
 		list += '</div>';
 		element.append(list);
@@ -140,28 +199,36 @@ function showVoraussetzungen(element) {
 }
 
 /**
- * Toggles the expansion/reduction of a course <div>, thus showing or hiding its children
- * @param {Object} element The element to expand/reduce
-**/
+ * Toggles the expansion/reduction of a course <div>, thus showing or hiding its
+ * children
+ * 
+ * @param {Object}
+ *            element The element to expand/reduce
+ */
 function expand_reduce(element) {
-	if (typeof element === "undefined" || !(typeof element === "object") || !(element.prop("tagName") == "DIV")) return;
-	var button = element.children("table.header").find("td.left.button.expander");
+	if (typeof element === "undefined" || !(typeof element === "object")
+			|| !(element.prop("tagName") == "DIV"))
+		return;
+	var button = element.children("table.header").find(
+			"td.left.button.expander");
 	if (button.attr("alt") == "plus") {
-		button.css("background-image", 'url("sites/all/modules/stukowin/images/Minus.png")');
+		button.css("background-image",
+				'url("sites/all/modules/stukowin/images/Minus.png")');
 		button.attr("alt", "minus");
 		button.attr("title", "Einklappen");
 	} else {
-		button.css("background-image", 'url("sites/all/modules/stukowin/images/Plus.png")');
+		button.css("background-image",
+				'url("sites/all/modules/stukowin/images/Plus.png")');
 		button.attr("alt", "plus");
 		button.attr("title", "Ausklappen");
 	}
 	var children = element.children("div.fach, div.modul, div.lv")
 	if (children.hasClass("hidden")) {
-		children.each(function () {
+		children.each(function() {
 			jQuery(this).slideDown("fast");
 		})
 	} else {
-		children.each(function () {
+		children.each(function() {
 			jQuery(this).slideUp("fast");
 		});
 	}
@@ -171,77 +238,102 @@ function expand_reduce(element) {
 
 /**
  * Expands all divs that are currently reduced
-**/
+ */
 function expand_all() {
-	jQuery(".reduced").each(function () {
+	jQuery(".reduced").each(function() {
 		expand_reduce(jQuery(this));
 	});
 }
 
 /**
- * Expands the curriculum tree down to the given element, scrolls to it and highlights it
- * @param {Object|String|Number} element The element to expand to. If an string or a number is given, it will scroll to the div with that id
- * @param {String} [highlightColor = '#90EE90'] The color to highlight the element in
-**/
+ * Reduces all divs that are currently expanded
+ */
+function reduce_all() {
+	jQuery("div.fach, div.modul").not(".reduced").each(function() {
+		expand_reduce(jQuery(this));
+	});
+}
+/**
+ * Expands the curriculum tree down to the given element, scrolls to it and
+ * highlights it
+ * 
+ * @param {Object|String|Number}
+ *            element The element to expand to. If an string or a number is
+ *            given, it will scroll to the div with that id
+ * @param {String}
+ *            [highlightColor = '#90EE90'] The color to highlight the element in
+ */
 function expandAndScrollToElement(element, highlightColor) {
 	if (typeof element === "number" || typeof element === "string") {
 		element = jQuery("div#" + element);
 	}
-	if (typeof element === "undefined") return;
+	if (typeof element === "undefined")
+		return;
 	if (typeof highlightColor === "undefined") {
 		highlightColor = "#90EE90";
 	}
-	element.parents(".reduced").each(function () {
+	element.parents(".reduced").each(function() {
 		expand_reduce(jQuery(this));
 	});
 	if (!isFullyVisible(element)) {
-		jQuery("html, body").animate({ scrollTop: element.offset().top }, { duration: 'slow', easing: 'swing' });
+		jQuery("html, body").animate({
+			scrollTop : element.offset().top
+		}, {
+			duration : 'slow',
+			easing : 'swing'
+		});
 	}
-	element.stop(true, true).effect("highlight", { color: highlightColor }, 3000);
+	element.stop(true, true).effect("highlight", {
+		color : highlightColor
+	}, 3000);
 }
 
 /**
  * Creates the <div>s for all the courses recursively.
- * @param {Object} kurs The course to create the <div> for
- * @param {Number} level The current recursion level (needed in order to decide if element is top-level or not
+ * 
+ * @param {Object}
+ *            kurs The course to create the <div> for
+ * @param {Number}
+ *            level The current recursion level (needed in order to decide if
+ *            element is top-level or not
  * @return {String} The complete HTML for the given course's <div>
-**/
+ */
 function createDivs(kurs, level, parentIsRoot) {
 	var div, hasVoraussetzungen, hasEmpfohlen, typ, rightTds;
-	
-	if("lva" in kurs){
+
+	if ("lva" in kurs && kurs["lva"]) {
 		rightTds = createTds(kurs);
-		if (!("children" in kurs)) {
-			//var clazz = parentIsRoot && parentIsRoot === true ? "" : " hidden"
-			div = '<div class="lv'+ /*clazz +*/ '" id="' + kurs["tid"] + '">'
-				+ '<table class="header"><tr>'
-				+ '<td class="left ects"><p class="center" >' + ("lva" in kurs ? kurs["lva"]["lvtypshort"] : "") + '</p></td>'
-				+ rightTds
-				+ '</tr></table></div>';
+		if (kurs["lva"]["lvatype"] == 3) {
+			div = '<div class="lv' + '" id="' + kurs["tid"] + '">'
+					+ '<table class="header"><tr>'
+					+ '<td class="left ects"><p class="center" >'
+					+ ("lva" in kurs ? kurs["lva"]["lvtypshort"] : "")
+					+ '</p></td>' + rightTds + '</tr></table></div>';
 		} else {
-			if (kurs["lva"] && kurs["lva"]["lvatype"] == 1) {
+			if (kurs["lva"]["lvatype"] == 1) {
 				typ = "fach";
-			} else if(!kurs["lva"]) {
-				typ = "fach";
-			} else {
+			} else if (kurs["lva"]["lvatype"] == 2) {
 				typ = "modul";
 			}
-			 div = '<div class="' + typ + '" id="' + kurs["tid"] + '">'
-				+ '<table class="header"><tr>'
-				+ '<td class="left button expander" alt="minus" title="Ausklappen"></td>'
-				+ rightTds
-				+ '</tr></table>';
+			div = '<div class="'
+					+ typ
+					+ '" id="'
+					+ kurs["tid"]
+					+ '">'
+					+ '<table class="header"><tr>'
+					+ '<td class="left button expander" alt="minus" title="Ausklappen"></td>'
+					+ rightTds + '</tr></table>';
 			if ("children" in kurs) {
-				jQuery.each(kurs["children"], function (key, val) {
+				jQuery.each(kurs["children"], function(key, val) {
 					div += createDivs(val, level + 1);
 				});
 			}
 			div += "</div>";
 		}
-	}else{
+	} else {
 		div = '<div class="abschnitt"><h1>' + kurs["name"] + '</h1></div>';
 		if ("children" in kurs) {
-			jQuery.each(kurs["children"], function (key, val) {
+			jQuery.each(kurs["children"], function(key, val) {
 				div += createDivs(val, level + 1, true);
 			});
 		}
@@ -251,23 +343,45 @@ function createDivs(kurs, level, parentIsRoot) {
 }
 
 /**
- * Convenience function for creating the table cells used in course and module divs.
- * @param {Object} kurs The course to create the cells for.
- **/
+ * Convenience function for creating the table cells used in course and module
+ * divs.
+ * 
+ * @param {Object}
+ *            kurs The course to create the cells for.
+ */
 function createTds(kurs) {
-	var anzVoraussetzungen = "voraussetzungen" in kurs ? kurs["lva"]["voraussetzungen"].length : 0;
-	var anzEmpfohlen = "empfohlen" in kurs ? kurs["lva"]["empfohlen"].length : 0;
-	var rightTds = '<td class="center"><p class="center">' + ("lva" in kurs ? kurs["lva"]["title"] : kurs["name"]) + '</p></td>'
-			+ '<td class="right button voraussetzung' + (anzVoraussetzungen ? "" : " empty") + '" title="' + (anzVoraussetzungen ? anzVoraussetzungen + " verpflichtende Voraussetzung" + (anzVoraussetzungen > 1 ? "en" : "") : "Keine verpflichtenden Voraussetzungen") + '"/>'
-			+ '<td class="right button empfohlen' + (anzEmpfohlen ? "" : " empty") + '" title="' + (anzEmpfohlen ? anzEmpfohlen + " empfohlene Voraussetzung" + (anzEmpfohlen > 1 ? "en" : "") : "Keine empfohlenen Voraussetzungen") + '"/>'
-			+ '<td class="right ects" title="ECTS"><p class="center">' + ("lva" in kurs ? kurs["lva"]["ects"] : "")+ '</p></td>';
+	var anzVoraussetzungen = "voraussetzungen" in kurs ? kurs["lva"]["voraussetzungen"].length
+			: 0;
+	var anzEmpfohlen = "empfohlen" in kurs ? kurs["lva"]["empfohlen"].length
+			: 0;
+	var rightTds = '<td class="center"><p class="center">'
+			+ ("lva" in kurs ? kurs["lva"]["title"] : kurs["name"])
+			+ '</p></td>'
+			+ '<td class="right button voraussetzung'
+			+ (anzVoraussetzungen ? "" : " empty")
+			+ '" title="'
+			+ (anzVoraussetzungen ? anzVoraussetzungen
+					+ " verpflichtende Voraussetzung"
+					+ (anzVoraussetzungen > 1 ? "en" : "")
+					: "Keine verpflichtenden Voraussetzungen")
+			+ '"/>'
+			+ '<td class="right button empfohlen'
+			+ (anzEmpfohlen ? "" : " empty")
+			+ '" title="'
+			+ (anzEmpfohlen ? anzEmpfohlen + " empfohlene Voraussetzung"
+					+ (anzEmpfohlen > 1 ? "en" : "")
+					: "Keine empfohlenen Voraussetzungen") + '"/>'
+			+ '<td class="right ects" title="ECTS"><p class="center">'
+			+ ("lva" in kurs ? kurs["lva"]["ects"] : "") + '</p></td>';
 	return rightTds;
 }
 
 /**
  * Utility to check if an element is fully visible
- * @param {Object} elem The element to check
-**/
+ * 
+ * @param {Object}
+ *            elem The element to check
+ */
 function isFullyVisible(elem) {
 	try {
 		var docViewTop = jQuery(window).scrollTop();
@@ -282,14 +396,14 @@ function isFullyVisible(elem) {
 
 function buildRequest(baseUrl, type, curriculums) {
 	var url = baseUrl;
-	if(baseUrl.indexOf("?") >= 0){
+	if (baseUrl.indexOf("?") >= 0) {
 		baseUrl += "&";
-	}else{
+	} else {
 		baseUrl += "?";
 	}
 	baseUrl += "currtype=";
 	baseUrl += type;
-	for(var i = 0; i < curriculums.length; i++){
+	for (var i = 0; i < curriculums.length; i++) {
 		baseUrl += "&taxtypes[]=";
 		baseUrl += curriculums[i];
 	}
